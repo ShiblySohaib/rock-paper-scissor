5c151ea1544df125cb7003a261d641dec90ddb92
ShiblySohaib <shiblysohaib@gmail.com>
2023-05-21 16:28:09 +0600

    format code

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const moves = ["rock", "paper", "scissor"];
 getComputerChoice = () => Math.floor(Math.random() * 3);
+
 function playerChoice() {
   let x = prompt("Enter your choice").toLowerCase();
-  // let x = y.toLowerCase();
   if (x == "rock") return 0;
   else if (x == "paper") return 1;
   else return 2;
 }
+
 function playRound(p = playerChoice(), c = getComputerChoice()) {
   if ((p + 1) % 3 == c) {
     console.log(moves[c] + " beats " + moves[p] + ". You lose");
@@ -19,6 +20,7 @@ function playRound(p = playerChoice(), c = getComputerChoice()) {
     return "w";
   }
 }
+
 function game() {
   let user = 0,
     pc = 0;
@@ -27,7 +29,7 @@ function game() {
     if (result == "w") user++;
     else if (result == "l") pc++;
   }
-  console.log(`You : ${user}, Computer: ${pc}, Draws: ${5-user-pc}`);
+  console.log(`You : ${user}, Computer: ${pc}, Draws: ${5 - user - pc}`);
   if (user > pc) console.log("Game finished. You win.");
   else if (pc > user) console.log("Game finished. Computer wins.");
   else console.log("Game finished. It's a draw.");
