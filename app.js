46ace42ab8631cd7d39c87cf951efbe7fad29116
Shibly Sohaib <shiblysohaib@gmail.com>
2023-06-06 13:14:59 +0530

    v 1.0

--- app.js
+++ app.js
@@ -1,43 +1,90 @@
 const moves = ["Rock", "Paper", "Scissor"];
+const gamePoint = 5;
 const playerMove = document.querySelector("#playermove");
 const computerMove = document.querySelector("#pcmove");
 const rpschoices = document.querySelectorAll(".choice");
+const gamescreen = document.querySelector("#gamescreen");
+const body = document.querySelector('body');
 
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissor = document.querySelector("#scissor");
 
+const pScore = document.querySelector("#playerScore");
+const cScore = document.querySelector("#computerScore");
+
 let playerChoice = null;
 let computerChoice = null;
 let announcement = document.querySelector("#announce");
 
+let playerScore = 0;
+let computerScore = 0;
+
+function initialize(){
+    playerScore = 0;
+    computerScore = 0;
+    announcement.textContent = "Choose your move!!"
+    pScore.innerHTML = `${playerScore}`;
+    cScore.innerHTML = `${computerScore}`;
+    gamescreen.style.display = 'block';
+    endscreen.style.display = 'none';
+}
+
+playerMove.innerHTML =
+    '<img src="img/qmark.png" style="width: 70%; height: 70%; border-radius: 10px;">';
+computerMove.innerHTML =
+    '<img src="img/qmark.png" style="width: 70%; height: 70%; border-radius: 10px;">';
+
 rpschoices.forEach((choice) => {
-  choice.addEventListener("click", function getPlayerChoice(e) {
-    playerMove.innerHTML = choice.innerHTML;
-    if(e.target == '#rock') playerChoice = 0;
-    else if(e.target == "#paper") playerChoice = 1;
-    else if(e.target == "#scissor") playerChoice = 2;
-    getComputerChoice();
-    playRound();
-  });
+    choice.addEventListener("click", function getPlayerChoice(e) {
+        console.log(e.target.parentElement.id);
+        playerMove.innerHTML = choice.innerHTML;
+        if (e.target.parentElement.id == "rock") playerChoice = 0;
+        else if (e.target.parentElement.id == "paper") playerChoice = 1;
+        else if (e.target.parentElement.id == "scissor") playerChoice = 2;
+        getComputerChoice();
+        playRound();
+    });
 });
 
-
-function getComputerChoice(){
-  let computerChoice =  Math.floor(Math.random() * 3);
-  if(computerChoice == 0) computerMove.innerHTML = rock.innerHTML;
-  if(computerChoice == 1) computerMove.innerHTML = paper.innerHTML;
-  if(computerChoice == 2) computerMove.innerHTML = scissor.innerHTML;
+function getComputerChoice() {
+    computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice == 0)
+        computerMove.innerHTML =
+            '<img src="img/crock.jpg" style="width: 100%; height: 100%; border-radius: 10px"/>';
+    if (computerChoice == 1)
+        computerMove.innerHTML =
+            '<img src="img/cpaper.jpg" style="width: 100%; height: 100%; border-radius: 10px"/>';
+    if (computerChoice == 2)
+        computerMove.innerHTML =
+            '<img src="img/cscissor.jpg" style="width: 100%; height: 100%; border-radius: 10px"/>';
 }
 
 function playRound() {
+    if ((playerChoice + 1) % 3 == computerChoice) {
+        announcement.innerHTML = "You lose!";
+        computerScore++;
+        cScore.innerHTML = `${computerScore}`;
+    } else if (playerChoice == computerChoice) {
+        announcement.innerHTML = "It's a draw!";
+    } else {
+        announcement.innerHTML = "You win!";
+        playerScore++;
+        pScore.innerHTML = `${playerScore}`;
+    }
+    if (playerScore == gamePoint || computerScore == gamePoint) endgame();
+}
 
-  if ((playerChoice + 1) % 3 == computerChoice) {
-    announcement.innerHTML = "You lose!"
-    alert(`${playerChoice}, ${computerChoice}`);
-  } else if (playerChoice == computerChoice) {
-    announcement.innerHTML = "It's a draw!"
-  } else {
-    announcement.innerHTML = "You win!"
-  }
+const endscreen = document.querySelector("#ends");
+const restart = document.querySelector(".restart");
+const result = document.querySelector("#result");
+endscreen.style.display = 'none';
+restart.onclick = () => initialize();
+
+
+function endgame() {
+    result.textContent = "You won the game!"
+    gamescreen.style.display = 'none';
+    endscreen.style.display = 'block';
+    endscreen.style.display = 'flex';
 }
